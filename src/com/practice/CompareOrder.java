31db14c5cb562cf9adbf72d6f700947e31f23330
ashishawasthi04 <ashishawasthi04@gmail.com>
2021-11-27 20:16:40 -0800

    Code formatting.

--- src/com/practice/CompareOrder.java
+++ src/com/practice/CompareOrder.java
@@ -1,6 +1,7 @@
 package com.practice;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CompareOrder {
     public static void main(String[] args) {
@@ -12,56 +13,53 @@ public class CompareOrder {
         earlyLogout(logs, 5);
     }
 
-    public static List<String> compareOrder(List<String> x, List<String> y){
+    public static List<String> compareOrder(List<String> x, List<String> y) {
         List<String> res = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < x.size(); i++){
+        for (int i = 0; i < x.size(); i++) {
             map.clear();
-            for(char c : x.get(i).toCharArray()){
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-            for(char c : y.get(i).toCharArray()){
-                map.put(c, map.getOrDefault(c, 0) - 1);
-            }
+            for (char c : x.get(i).toCharArray())
+                map.merge(c, 1, Integer::sum);
+
+            for (char c : y.get(i).toCharArray())
+                map.merge(c, -1, Integer::sum);
+
             long count = map.values().stream().filter(val -> Math.abs(val) > 3).count();
-            if(count == 0) res.add("YES");
-            else res.add("NO");
+            res.add(count == 0 ? "YES" : "NO");
         }
         res.forEach(System.out::println);
         return res;
     }
 
-    public static List<Integer> warehouse(int[] shelves){
+    public static List<Integer> warehouse(int[] shelves) {
         List<Integer> res = new ArrayList<>();
         int[] counter = new int[1];
         Map<Integer, Integer> map = new HashMap<>();
         Arrays.stream(shelves).distinct().boxed()
                 .sorted(Comparator.naturalOrder())
                 .forEach(shelf -> map.put(shelf, ++counter[0]));
-        for(int shelf : shelves){
+        for (int shelf : shelves) {
             res.add(map.get(shelf));
         }
         res.forEach(System.out::println);
         return res;
     }
 
-    public static List<String> earlyLogout(List<String> logs, int maxTime){
+    public static List<String> earlyLogout(List<String> logs, int maxTime) {
         Map<String, int[]> map = new HashMap<>();
-        List<String> res = new ArrayList<>();
         logs.forEach(log -> {
-            String[] record = log.split(" ");
-            if(!map.containsKey(record[0])) map.put(record[0], new int[2]);
-            if("sign-in".equals(record[1]))
-                map.get(record[0])[0] = Integer.parseInt(record[2]);
-            else
-                map.get(record[0])[1] = Integer.parseInt(record[2]);
+            String[] record = log.split("\\s+");
+            if (!map.containsKey(record[0])) map.put(record[0], new int[2]);
+            boolean isSignIn = "sign-in".equals(record[1]);
+            map.get(record[0])[isSignIn ? 0 : 1] = Integer.parseInt(record[2]);
         });
 
-        map.forEach((key, value) -> {
-            if(value[0] != 0 && value[1] != 0 && value[1] - value[0] < maxTime)
-                res.add(key);
-        });
-        res.sort(Comparator.comparing(Integer::parseInt));
+        List<String> res = map.entrySet().stream().filter(entry -> {
+                    int[] value = entry.getValue();
+                    return (value[0] * value[1] != 0 && value[1] - value[0] < maxTime);
+                }).map(Map.Entry::getKey)
+                .sorted(Comparator.comparing(Integer::parseInt))
+                .collect(Collectors.toList());
         res.forEach(System.out::println);
         return res;
 
